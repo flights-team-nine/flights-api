c8110e4b185217033d1ef693f72742eb6b40571b
damishra <dxm2269@rit.edu>
2021-04-15 15:08:31 -0400

    finished veterans

--- src/controllers/veterans.py
+++ src/controllers/veterans.py
@@ -45,3 +45,51 @@ def create_veteran(db: Session, fname: str, mi: str, lname: str, guardian: int, 
     db.refresh(medical_sheet)
     db.refresh(veteran)
     return veteran
+
+
+def update_veteran(db: Session, id: int, fname=None, mi=None, lname=None, guardian=None, details=None, contact=None, medical=None):
+    veteran = db.query(Veteran).get(id)
+    contact_sheet = db.query(ContactSheet).filter(
+        ContactSheet.user == veteran.id).first()
+    medical_sheet = db.query(MedicalSheet).filter(
+        MedicalSheet.user == veteran.id).first()
+    if fname != None or mi != None or lname != None or guardian != None or details != None or medical != None or contact != None:
+        if fname != None:
+            veteran.first_name = fname
+        if lname != None:
+            veteran.last_name = lname
+        if mi != None:
+            veteran.middle_initial = mi
+        if guardian != None:
+            veteran.guardian = guardian
+        if details != None:
+            veteran.details = details
+        if medical != None:
+            medical_sheet.training = medical["training"]
+            medical_sheet.conditions = medical["conditions"]
+            medical_sheet.diet = medical["diet"]
+            medical_sheet.comments = medical["comments"]
+        if contact != None:
+            contact_sheet.primary_phone = contact["p_phone"]
+            contact_sheet.primary_addr = contact["p_addr"]
+            contact_sheet.phones = contact["phones"]
+            contact_sheet.emergency = contact["emergency"]
+            contact_sheet.email = contact["email"]
+        db.commit()
+        db.flush()
+        db.refresh(veteran)
+        db.refresh(contact_sheet)
+        db.refresh(medical_sheet)
+    return veteran
+
+
+def delete_veteran(db: Session, id: int):
+    veteran = db.query(Veteran).get(id)
+    medical = db.query(MedicalSheet).filter(MedicalSheet.user == id).first()
+    contact = db.query(ContactSheet).filter(ContactSheet.user == id).first()
+    db.delete(medical)
+    db.delete(contact)
+    db.delete(veteran)
+    db.commit()
+    db.flush()
+    return veteran.id
