1b8396d259070198163a09c73634c733c61f8d0f
damishra <dxm2269@rit.edu>
2021-04-20 12:15:07 -0400

    final

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+from src.resources.veterans import VeteranResource
+from src.models.veterans import Veteran
+from src.resources.busses import BusResource
+from src.resources.teams import TeamsResource
 from flask import Flask
 from flask_restful import Api
 from src.utilities.database import Base, engine, SessionLocal
@@ -14,6 +18,9 @@ app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(AuthResource, "/auth")
+api.add_resource(TeamsResource, "/teams/<id:int>")
+api.add_resource(BusResource, "/bus")
+api.add_resource(VeteranResource, "/veteran/<id:int>")
 
 
 if __name__ == '__main__':
